8a9ac6287a817c321b42d48c43f833e0d71125ca
NoorEldenWael <your_email@example.com>
2026-03-08 02:13:14 +0200

    intitial prject structure

--- lib/core/constants/app_endpoints.dart
+++ lib/core/constants/app_endpoints.dart
@@ -1,4 +1,6 @@
-class AppEndpoints {
-  AppEndpoints._();
-  // static const String login = "login";
+class AppEndPoints {
+  AppEndPoints._();
+  static const String baseUrl = 'https://ecommerce.routemisr.com/api/v1';
+  static const String getProductsEndpoint = '$baseUrl/products';
+  static const String getCategoriesEndpoint = '$baseUrl/categories';
 }
