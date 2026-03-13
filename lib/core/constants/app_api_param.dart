3b5eba772e485d26afdef2b6c762e356415a90af
abanoub al3bani <apanouprizkalla@gmail.com>
2026-03-13 08:46:34 +0200

    git commit -m "Fix: apply comments on development branch"

--- lib/core/constants/app_api_param.dart
+++ lib/core/constants/app_api_param.dart
@@ -1,4 +1,12 @@
 class AppApiParam {
   AppApiParam._();
-  // static const String limt = "limt";
+
+  static const String username = "username";
+  static const String firstName = "firstName";
+  static const String lastName = "lastName";
+  static const String email = "email";
+  static const String password = "password";
+  static const String rePassword = "rePassword";
+  static const String phone = "phone";
+  static const String token = "token";
 }
