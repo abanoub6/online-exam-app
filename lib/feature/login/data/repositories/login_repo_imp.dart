0141c564ca00a2286d343bceab2db6f8217f876e
Noor Elden Wael <154272325+NoorEldenWael@users.noreply.github.com>
2026-03-26 17:32:46 +0200

    Add remember me functionality to login feature
    - Create RememberMeUseCase for managing user preferences
    - Add rememberMe field to LoginRequest model
    - Extend LoginRepoContract with rememberMe and isRememberedMe methods
    - Implement remember me storage using FlutterSecureStorage
    - Add rememberMe constant to AppApiParam

--- lib/feature/login/data/repositories/login_repo_imp.dart
+++ lib/feature/login/data/repositories/login_repo_imp.dart
@@ -1,8 +1,11 @@
+import 'dart:developer';
+
+import 'package:flutter_secure_storage/flutter_secure_storage.dart';
 import 'package:injectable/injectable.dart';
 import 'package:online_exam_app_v/config/base_responce/base_responce.dart';
+import 'package:online_exam_app_v/core/constants/app_api_param.dart';
 import 'package:online_exam_app_v/feature/login/data/datasources/login_remote_data_source_contract.dart';
 import 'package:online_exam_app_v/feature/login/data/models/login_request.dart';
-import 'package:online_exam_app_v/feature/login/data/models/login_response_model.dart';
 import 'package:online_exam_app_v/feature/login/data/models/user_model.dart';
 import 'package:online_exam_app_v/feature/login/domain/entities/user_entity.dart';
 import 'package:online_exam_app_v/feature/login/domain/repositories/login_repo_contract.dart';
@@ -15,6 +18,8 @@ class LoginRepoImp implements LoginRepoContract {
   Future<BaseResponse<UserEntity>> login(LoginRequest loginRequest) async {
     BaseResponse<UserModel> response = await loginRemoteDataSourceContract
         .login(loginRequest);
+
+    log(response.runtimeType.toString());
     switch (response) {
       case SuccessBaseResponse<UserModel>():
         return SuccessBaseResponse<UserEntity>(data: response.data.toEntity());
@@ -23,5 +28,22 @@ class LoginRepoImp implements LoginRepoContract {
           errorMessage: response.errorMessage,
         );
     }
+  }
+
+  @override
+  void rememberMe(bool rememberMe) {
+    FlutterSecureStorage().write(
+      key: AppApiParam.rememberMe,
+      value: rememberMe.toString(),
+    );
+  }
+
+  @override
+  Future<bool?> isRememberedMe() async {
+    final result = await FlutterSecureStorage().read(
+      key: AppApiParam.rememberMe,
+    );
+    if (result != null) return bool.parse(result);
+    return null;
   }
 }
