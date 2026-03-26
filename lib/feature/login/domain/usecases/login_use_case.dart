7caca0250850d9a28430a798adba7a4413ed0dee
Noor Elden Wael <154272325+NoorEldenWael@users.noreply.github.com>
2026-03-26 17:34:47 +0200

    update injection case of login_use_case

--- lib/feature/login/domain/usecases/login_use_case.dart
+++ lib/feature/login/domain/usecases/login_use_case.dart
@@ -1,12 +1,11 @@
 import 'package:injectable/injectable.dart';
 import 'package:online_exam_app_v/config/base_responce/base_responce.dart';
 import 'package:online_exam_app_v/feature/login/data/models/login_request.dart';
-import 'package:online_exam_app_v/feature/login/data/models/login_response_model.dart';
 import 'package:online_exam_app_v/feature/login/domain/entities/user_entity.dart';
 import 'package:online_exam_app_v/feature/login/domain/repositories/login_repo_contract.dart';
 
 
-@injectable
+@lazySingleton
 class LoginUseCase {
   LoginUseCase(this.loginRepoContract);
   LoginRepoContract loginRepoContract;
